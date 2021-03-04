e6bff47aafe6cf00c7f1c34c63e2a76dc1e5ac71
Shagon <goshan58rus@gmail.com>
2021-03-04 14:52:53 +0300

    Обновил зависимость PyJWT, проблемы со сборкой Сделал авторизацию и шаблон для пост-логин страницы

--- front/lk.py
+++ front/lk.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 import publishing_platform.auth.service as auth_service
-
+from publishing_platform.constants import BACKEND_DOMAIN
 
 default_routes = APIRouter()
 
@@ -15,5 +15,5 @@ async def main(request: Request):
 
 @default_routes.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
-    return templates.TemplateResponse("login_form.html", {'request': request})
+    return templates.TemplateResponse("login_form.html", {'request': request, 'BACKEND_DOMAIN': BACKEND_DOMAIN})
 
--- publishing_platform/constants.py
+++ publishing_platform/constants.py
@@ -21,4 +21,4 @@ JWT_SECRET = "5a6a80f5c776eb849bdfb58cdd33f1f74fe86923a7b0dd64816dc6313d18c27f"
 JWT_ALGORITHM = "HS256"
 JWT_EXP_DELTA_SECONDS = 30
 
-BACKEND_DOMAIN = os.environ.get("BACKEND_DOMAIN", "http://localhost:8085/%22")
+BACKEND_DOMAIN = os.environ.get("BACKEND_DOMAIN", "http://localhost:8083/")
